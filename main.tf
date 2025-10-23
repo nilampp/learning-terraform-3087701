f90693b541040e267114ec56dfde18e8f41cd778
nilampp <nilampatil2512@gmail.com>
2025-10-23 12:36:01 -0700

    fixing the syntax

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ resource "aws_security_group_rule" "rule_http_in"{
   from_port   = 443 
   to_port     = 443
   protocol    = "tcp"
-  cidr-blocks = ["0.0.0.0/0"]
+  cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.security_group.id
 }
@@ -50,7 +50,7 @@ resource "aws_security_group_rule" "rule_http_everything_out"{
   from_port   = 0 
   to_port     = 0
   protocol    = "-1"
-  cidr-blocks = ["0.0.0.0/0"]
+  cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.security_group.id
 }

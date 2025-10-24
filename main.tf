2aa9c177ac2161f60b8b4c69a0c6ba33c9aba2fd
nilampp <nilampatil2512@gmail.com>
2025-10-24 10:54:18 -0700

    corrected VPC id

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ module "module_security_group"{
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.3.1"
 
-  vpc_ids = data.aws_vpc.default.id
+  vpc_id = data.aws_vpc.default.id
 
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]

f5c5dfb02b3b800655a1a94f76410b37748119ac
nilampp <nilampatil2512@gmail.com>
2025-10-24 13:25:56 -0700

    create new dev vpc

--- main.tf
+++ main.tf
@@ -18,11 +18,29 @@ data "aws_vpc" "default"{
   default = true
 }
 
+module "module_dev_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev_vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
+
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
  vpc_security_group_ids = [module.module_security_group.security_group_id]
+
+ subnet_id = module.module_dev_vpc.public_subnets[0]
 
   tags = {
     Name = "Learning Terraform"
@@ -34,7 +52,7 @@ module "module_security_group"{
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.3.1"
 
-  vpc_id = data.aws_vpc.default.id
+  vpc_id = module.module_dev_vpc.vpc_id
 
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
